1e2d92b30efd5b07d8fb07371b0a18cc9eba9fe1
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-14 18:03:53 +0200

    added application

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import the Flask module
+from flask import Flask
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Define a route for the root URL ("/") with a function to handle requests
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+# Start the Flask app if this file is run directly
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
