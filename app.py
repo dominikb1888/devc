8927a90ed9c168222ef7f32254cc4f176dd86fe9
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-14 21:55:12 +0200

    added postgres connections and retrieval of hello world from db

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-# Import the Flask module
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-# Create an instance of Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@db/mydb'  # Update with your own database connection details
+db = SQLAlchemy(app)
 
-# Define a route for the root URL ("/") with a function to handle requests
-@app.route('/')
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255))
+
+    def __init__(self, content):
+        self.content = content
+
+@app.route('/api/hello')
 def hello():
-    return 'Hello, World!'
+    # Retrieve message from database
+    message = Message.query.get(1)
+    if message:
+        return jsonify(message=message.content)
+    else:
+        return jsonify(message='No message found in the database.')
 
-# Start the Flask app if this file is run directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
