c99adf5e7cb1e09145e382fc1d70ef1ee0bdc862
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-14 21:58:52 +0200

    added Dockerfiles for vue flask and postgres (via docker-compose)

---
+++ .devcontainer/Dockerfile.vue
@@ -0,0 +1,20 @@
+# Use the official Node.js image as the base image
+FROM node:14-alpine
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY vue/package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code to the working directory
+COPY vue/ .
+
+# Expose port 8080 for the Vue.js app to listen on
+EXPOSE 8080
+
+# Start the Vue.js app
+CMD ["npm", "run", "serve"]
