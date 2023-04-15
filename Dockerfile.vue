cca403039e624a07c6350f663d04117ad82aeb6e
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-15 11:32:01 +0200

    used ms codespaces containers

--- Dockerfile.vue
+++ Dockerfile.vue
@@ -1,5 +1,5 @@
 # Use the official Node.js image as the base image
-FROM node:14-alpine
+FROM mcr.microsoft.com/vscode/devcontainers/javascript-node:18-buster
 
 # Set the working directory in the container
 WORKDIR /app
