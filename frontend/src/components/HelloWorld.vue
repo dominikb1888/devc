31fb0776e733bf564a6e773d60e00f02baa8a625
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-15 13:32:52 +0200

    added local and cloud dev docker config

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+// defineProps<{
+//   msg: string
+
+export default {
+  data() {
+    return {
+      backendMessage: ''
+    };
+  },
+  mounted() {
+    // Fetch backend response from Flask API
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => {
+        this.backendMessage = data.message;
+      })
+      .catch(error => {
+        console.error('Error fetching backend data:', error);
+      });
+  }
+};
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ backendMessage }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
